7f4930e135c70f45a811c7fbfe0f7c20b5b43ad6
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2026-06-16 15:45:23 -0700

    wip

--- gen-diff.sh
+++ gen-diff.sh
@@ -2,17 +2,30 @@
 
 set -e
 
-rm -r target && mvn package -Dmaven.test.skip=true && mv target/ original && mvn package -Dmaven.test.skip=true
+rm -rf original
+rm -rf rebuild
+
+mvn clean
+mvn package -Dmaven.test.skip=true
+mv target/ original
+
+mvn clean
+mvn package -Dmaven.test.skip=true
+mv target/ rebuild
+
+CLASSIFIER=""
+#CLASSIFIER="-slim"
+
 pushd original
-unzip clojure-1.12.5-slim.jar -d clojure-1.12.5-slim
+unzip clojure-1.12.5$CLASSIFIER.jar -d clojure-1.12.5$CLASSIFIER
 popd
 pushd rebuild
-unzip clojure-1.12.5-slim.jar -d clojure-1.12.5-slim
+unzip clojure-1.12.5$CLASSIFIER.jar -d clojure-1.12.5$CLASSIFIER
 popd
 # raw bytecode diff (procyon not on PATH, defaults to bytecode)
-diffoscope original/clojure-1.12.5-slim/clojure/lang/Var.class rebuild/clojure-1.12.5-slim/clojure/lang/Var.class > var.class.raw.diff
+diffoscope original/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class rebuild/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class > var.class.raw.diff
 # decompiled
-PATH="$PATH:bin" diffoscope original/clojure-1.12.5-slim/clojure/lang/Var.class rebuild/clojure-1.12.5-slim/clojure/lang/Var.class > var.class.diff
-javap -c original/clojure-1.12.5-slim/clojure/lang/Var.class > original/clojure-1.12.5-slim/clojure/lang/Var.class.javap
-javap -c rebuild/clojure-1.12.5-slim/clojure/lang/Var.class > rebuild/clojure-1.12.5-slim/clojure/lang/Var.class.javap
-diffoscope original/clojure-1.12.5-slim/clojure/lang/Var.class.javap rebuild/clojure-1.12.5-slim/clojure/lang/Var.class.javap > var.javap.diff
+PATH="$PATH:bin" diffoscope original/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class rebuild/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class > var.class.diff
+javap -c original/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class > original/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class.javap
+javap -c rebuild/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class > rebuild/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class.javap
+diffoscope original/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class.javap rebuild/clojure-1.12.5$CLASSIFIER/clojure/lang/Var.class.javap > var.javap.diff

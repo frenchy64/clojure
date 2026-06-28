68ec965d53e69d8034114ce4fcc4c892aa6b8487
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2026-06-27 23:53:26 -0700

    allow fail

--- compare-optimization.sh
+++ compare-optimization.sh
@@ -7,6 +7,6 @@ mvn --batch-mode --no-transfer-progress clean install -Dmaven.test.skip=true
 mvn --batch-mode --no-transfer-progress clean verify artifact:compare -Dmaven.test.skip=true -PoptimizeSyntaxQuote || true
 
 cat target/clojure-1.13.0-syntaxquotedvec.buildcompare
-diffoscope target/reference/org.clojure/clojure-1.13.0-syntaxquotedvec.jar target/clojure-1.13.0-syntaxquotedvec.jar > clojure-1.13.0-syntax-quotedvec.jar.diffoscope
+diffoscope target/reference/org.clojure/clojure-1.13.0-syntaxquotedvec.jar target/clojure-1.13.0-syntaxquotedvec.jar || true > clojure-1.13.0-syntax-quotedvec.jar.diffoscope
 
 cat clojure-1.13.0-syntax-quotedvec.jar.diffoscope
